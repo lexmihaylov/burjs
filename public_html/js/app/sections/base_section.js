fcbf359a2139743db40b9945e90d450479f02731
macosx <macosx@10.10.1.108>
2014-01-15 12:18:22 +0200

    Changed _super method to accept a array of arguments

--- public_html/js/app/sections/base_section.js
+++ public_html/js/app/sections/base_section.js
@@ -7,7 +7,7 @@ define(['libs/kage'], function(kage) {
     var BaseSection = kage.Class({
         extends: kage.Section,
         constructor: function(tag) {
-            BaseSection._super(this, tag);
+            BaseSection._super(this, [tag]);
         }
     });
 
