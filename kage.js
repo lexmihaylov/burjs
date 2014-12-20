a62afa4e7cd16f5a9851ea7d0ec81324b479885f
Alexander Mihaylov <lex.mihaylov@gmail.com>
2014-12-20 15:01:12 +0000

    updated template compilation precedure

--- kage.js
+++ kage.js
@@ -1538,22 +1538,12 @@ kage.View = kage.Class({
 });
 
 /**
- * Holds compiled application templates
- * @var Templates {HashMap}
- * @static
- */
-kage.View.Templates = new kage.util.HashMap();
-
-/**
  * Creates an instance of View
  * 
  * @static
- * @param {object} opt optional option parameter
- `opt: {
- url: '<some url>'
- context: <some context object>
- }`
- * @return {View}
+ * @param {String} template
+ * @param {Object} context(optional)
+ * @return {Component}
  */
 kage.View.make = function(template, context) {
     return new kage.View(template, context);
@@ -1563,7 +1553,7 @@ kage.View.make = function(template, context) {
  * Compiles and renders the compiled template to html
  * 
  * @param {object} variables variables to pass to the template
- * @return {Component}
+ * @return {String}
  */
 kage.View.prototype.render = function(variables) {
     var template = null;
@@ -1578,67 +1568,107 @@ kage.View.prototype.render = function(variables) {
 };
 
 /**
+ * Tmplate regular expression settings
+ * @var {Object}
+ */
+kage.View.settings = {
+    interpolate: /<%=([\s\S]+?)%>/g,
+    escape: /<%-([\s\S]+?)%>/g,
+    evaluate: /<%([\s\S]+?)%>/g
+};
+
+/**
+ * Template special chars escape map
+ * @var {Object}
+ */
+kage.View.escapes = {
+    "'": "'",
+    '\\': '\\',
+    '\r': 'r',
+    '\n': 'n',
+    '\u2028': 'u2028',
+    '\u2029': 'u2029'
+};
+
+/**
+ * Template special char regualt expression
+ * @var {RegExp}
+ */
+kage.View.escaper = /\\|'|\r|\n|\u2028|\i2028/g;
+
+
+/**
+ * Html entities map used for escaping html
+ * @var {Object}
+ */
+kage.View.htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+}
+
+/**
+ * Escapes an html string using the html entity map
+ * @param {String}
+ * @return {String}
+ * @static
+ */
+kage.View.escapeHtml = function(html) {
+    return String(html).replace(/[&<>"'\/]/g, function (entity) {
+        return kage.View.htmlEntities[entity];
+    });
+};
+
+/**
  * Compiles a template to javascript code
  * 
  * @static
  * @param {string} html The template code
  * @return {function} compiled template
  */
-kage.View.Compile = function(templateSource) {
-    // John Resig - http://ejohn.org/ - MIT Licensed
-    if (!templateSource) {
-        templateSource = '';
+kage.View.Compile = function(template) {
+
+    var matcher = new RegExp([
+            (kage.View.settings.interpolate).source,
+            (kage.View.settings.escape).source,
+            (kage.View.settings.evaluate).source
+        ].join('|') + '|$', 'g');
+
+    var source = '',
+        index = 0;
+    template.replace(matcher, function(match, interpolate, escape, evaluate, offset) {
+        source += template.slice(index, offset).replace(kage.View.escaper, function(match) {
+            return '\\' +kage.View.escapes[match];
+        });
+
+        index = offset + match.length;
+
+        if(interpolate) {
+            source += "'+((__v=" + interpolate + ")==null?'':__v)+'";
+        } else if(escape) {
+            source += "'+((__v=" + escape + ")==null?'':kage.View.escapeHtml(__v))+'";
+        } else if(evaluate) {
+            source += "'; " + evaluate + " __s+='";
+        }
+
+        return match;
+    });
+
+    var source = "var __v,__s=''; with(obj||{}){ __s+='" + source + "'; }; return __s;";
+
+    try {
+        render = new Function('obj, kage', source);
+    } catch (e) {
+        e.source = source;
+        throw e;
     }
 
-    var templateFunc = new Function(
-            "vars",
-            (
-                    "var p=[],print=function(){p.push.apply(p,arguments);};" +
-                    // Introduce the data as local variables using with(){}
-                    "if(!vars){vars={};}" +
-                    "with(vars){p.push('" +
-                    // Convert the template into pure JavaScript
-                    templateSource
-                    .replace(/[\r\t\n]/g, " ")
-                    .split("<%").join("\t")
-                    .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-                    .replace(/\t=(.*?)%>/g, "',$1,'")
-                    .split("\t").join("');")
-                    .split("%>").join("p.push('")
-                    .split("\r").join("\\'") +
-                    "');}return p.join('');"
-                    )
-            );
-
-    return templateFunc;
-};
-
-/**
- * Clears the template cache
- * 
- * @static
- */
-kage.View.clearCache = function() {
-    kage.View.Templates = new kage.util.HashMap();
-};
-
-/**
- * Compiles and adds the template to the template map or gets a compiled template
- * by its ID
- * @param {string} id identification of the compiled template. The compiled
- * template can be accessed by `kage.Cache('<key>')`
- * @param {string} template the teplates string
- * @returns {function} compiled template
- */
-kage.View.Cache = function(id, template) {
-    if(!template) {
-        return kage.View.Templates.get(id);
-    }
-    
-    var compiledTemplate = kage.Compile(template);
-    kage.View.Templates.add(id, compiledTemplate);
-    
-    return compiledTemplate;
+    return function(vars) {
+        return render.call(this, vars, kage);
+    };
 };
 
 return kage._init();
